2948e910a641def985594c5d3b9625741387dd5b
safaranees <aneesparvin08@gmail.com>
2019-06-09 21:35:45 +0530

    Create roman.py

---
+++ roman.py
@@ -0,0 +1,4 @@
+roman=input()
+number=["0","I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","XIII","XIV","XV","XVI","XVII","XVIII","XIX","XX"]
+if roman in number:
+  print(number.index(roman))
